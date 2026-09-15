e7298dc6c169046e1657913ea7015cf98328c284
Laska Yana <yanalasks@gmail.com>
2026-09-15 22:26:31 +0300

    refactor: update student module variable names according to Step 1.4

---
+++ labs/lab01/main.py
@@ -0,0 +1,18 @@
+import os
+import sys
+
+sys.path.append(
+    os.path.abspath(os.path.join(os.path.dirname(__file__), "../../"))
+)
+
+from shared.student import GROUP_NAME, STUDENT_NAME, VARIANT_NUMBER
+
+
+def main():
+    print(
+        f"Лабораторна робота №1 | {STUDENT_NAME}, група {GROUP_NAME}, варіант {VARIANT_NUMBER}"
+    )
+
+
+if __name__ == "__main__":
+    main()
