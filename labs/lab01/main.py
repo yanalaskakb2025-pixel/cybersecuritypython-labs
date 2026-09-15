81545147f4d833d6c9af56cafcc1e5a05aa5c805
Laska Yana <yanalasks@gmail.com>
2026-09-15 22:29:57 +0300

    style: format code with ruff according to PEP-8 standards

--- labs/lab01/main.py
+++ labs/lab01/main.py
@@ -1,9 +1,7 @@
 import os
 import sys
 
-sys.path.append(
-    os.path.abspath(os.path.join(os.path.dirname(__file__), "../../"))
-)
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../")))
 
 from shared.student import GROUP_NAME, STUDENT_NAME, VARIANT_NUMBER
 
